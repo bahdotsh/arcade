30d83e890626b7967b21d376d2c726728c2d5d91
bahdotsh <appu.yess@gmail.com>
2025-03-23 13:50:12 +0530

    title changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ import games from "../games";
 const Menu = ({ onSelectGame }) => {
   return (
     <div className="menu-container">
-      <h1>React Arcade</h1>
+      <h1>Arcade</h1>
       <div className="game-list">
         {Object.keys(games).map((gameKey) => (
           <div
